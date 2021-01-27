87e2aefe423bf386125334b8b02c3337ece98478
chitra gona <chitra.gona@zemosolabs.com>
2021-01-27 07:02:17 +0530

    assignment1
    Signed-off-by: chitra gona <chitra.gona@zemosolabs.com>

---
+++ assignment1.js
@@ -0,0 +1,6 @@
+//define and give value to the variables at a time
+var book1 = "Bee in the Hive"
+var book2 = "The Secret Life of the Ant"
+var book3 = "Laptops.Not Just for Breakfast Anymore"
+var book4 = "If I were a Superhero"
+var book5 = "T"
