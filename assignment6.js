249842da3da428a377fb5b2847538f9d57bb52e0
chitra gona <chitra.gona@zemosolabs.com>
2021-01-27 07:10:30 +0530

    assignment6
    Signed-off-by: chitra gona <chitra.gona@zemosolabs.com>

---
+++ assignment6.js
@@ -0,0 +1,12 @@
+var favColor = "blue"
+
+switch(favColor) {
+    case("blue") :
+    console.log("Violets are blue")
+    break
+    case("red") :
+    console.log("Roses are red")
+    break
+    default :
+    console.log(favColor)
+}
