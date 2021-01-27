da4063e371b652fb3af5b6f6d5dd88f3e7957221
chitra gona <chitra.gona@zemosolabs.com>
2021-01-27 07:04:55 +0530

    assignment4
    Signed-off-by: chitra gona <chitra.gona@zemosolabs.com>

---
+++ assignment4.js
@@ -0,0 +1,17 @@
+let answer1 = "bookx"
+let answer2 = "booky"
+let answer3 = "bookz"
+let answer4 = "booka"
+let answer5 = "bookb"
+let answer6 = "bookc"
+let arr =[]
+arr.push(answer1)
+arr.push(answer2)
+arr.push(answer3)
+arr.push(answer4)
+arr.push(answer5)
+arr.unshift(answer6)
+console.log(arr)
+
+
+console.log(arr)
